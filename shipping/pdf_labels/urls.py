2fb6ea4860f865e96b60f101c6e30be349c39eec
Sayan Banerjee <122210469+Sayan220@users.noreply.github.com>
2024-12-16 23:12:07 +0530

    Update urls.py

--- shipping/pdf_labels/urls.py
+++ shipping/pdf_labels/urls.py
@@ -5,4 +5,5 @@ from pdf_labels import views
 urlpatterns = [
     path("",views.index, name="index"),
     path("generate/",views.generate_pdf, name="generate_pdf"),
+    path('send-email/', views.send_email_addr, name='send_email_addr'),
 ]
